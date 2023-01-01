fab54329ef870eae27522e8c78f93c31e98cfec6
OliGarc333 <ngarcia333@gmail.com>
2023-01-01 21:52:44 +0100

    First commit

---
+++ client/src/Pages/ChatPage.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const ChatPage = () => {
+  const [chats, setChats] = useState();
+
+  const fetchChats = async () => {
+    const data = await axios.get('/api/chat');
+
+    setChats(data.data);
+  };
+
+  console.log(chats);
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  return (
+    <div>
+      {console.log(chats)}
+      {/* {chats.map(chat => (
+        <div>{chat.chatName}</div>
+      ))} */}
+      <h1>Chats</h1>
+    </div>
+  );
+};
+
+export default ChatPage;
---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const connectDB = require('./config/db.js');
+const dotenv = require('dotenv').config();
+const { chats } = require('./data/data.js');
+const userRoutes = require('./Routes/userRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+
+connectDB();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send(`API is running successfully`);
+});
+
+app.use('/api/user', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+  //   console.log(req.params.id);
+  const singleChat = chats.find(c => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+app.listen(5000, console.log('Server started on PORT 5000'));
