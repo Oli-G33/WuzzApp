0fe984230056ece7ae5536aaebba816a331fbb33
OliGarc333 <ngarcia333@gmail.com>
2023-01-16 21:02:20 +0100

    Changed Controllers file name for deployment

--- server/Routes/messageRoutes.js
+++ server/Routes/messageRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {
   sendMessage,
   allMessages
-} = require('../Controllers/messageControllers');
+} = require('../controllers/messageControllers');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
