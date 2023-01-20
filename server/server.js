07108f70844470dba9777f831b8468dda4fb6c94
OliGarc333 <ngarcia333@gmail.com>
2023-01-20 09:27:49 +0100

    More deployment changes

--- server/server.js
+++ server/server.js
@@ -24,17 +24,11 @@ app.use('/api/message', messageRoutes);
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname1, '/client/build')));
+app.use(express.static(path.join(__dirname1, '/client/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running..');
-  });
-}
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
+);
 
 app.use(notFound);
 app.use(errorHandler);
