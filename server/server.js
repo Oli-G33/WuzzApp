c0bfdcd168c29b7afea97fba70a27bc242b7ee32
OliGarc333 <ngarcia333@gmail.com>
2023-01-25 21:39:23 +0100

    Deploying on Netlify

--- server/server.js
+++ server/server.js
@@ -33,20 +33,6 @@ app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname1, '/client/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running..');
-  });
-}
-
 app.use(notFound);
 app.use(errorHandler);
 
@@ -63,7 +49,7 @@ app.get('/api/chat/:id', (req, res) => {
 const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
-  cors: { origin: 'https://wuzzapp.onrender.com' }
+  cors: { origin: 'https://wuzzapp1.netlify.app/' }
 });
 
 io.on('connection', socket => {
